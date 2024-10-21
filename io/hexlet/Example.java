8b33ddc89d2fe9c101e5911fa2efe51090bb585e
Pakk <mipakk95@gmail.com>
2024-10-21 22:16:04 +0300

    Добавление класса Example с использованием StringUtils и обработка строки

--- io/hexlet/Example.java
+++ io/hexlet/Example.java
@@ -3,9 +3,10 @@
 
      import org.apache.commons.lang3.StringUtils;
 
-     public class Example {
-         public static void main(String[] args) {
-             var capitalizedWord = StringUtils.capitalize("hexlet");
-             System.out.println(capitalizedWord);
-         }
-     }
+public class Example {
+    public static void main(String[] args) {
+        String word = "Hexlet";
+        String reversedWord = StringUtils.reverse(word);
+        System.out.println(reversedWord);
+    }
+}
